17fa5e4c2fe81a6e81883b9c409e8c54e28dc687
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-07-30 21:55:42 +0530

    Introduction to JavaScript

--- lesson-54/script.js
+++ lesson-54/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+alert("boom");
+console.log("meow! 1")
+console.log("meow! 2")
+console.log("meow! 3")
+
+var a = prompt("Enter your number")
+
+var isTrue = confirm("Are you sure you wanna blast your PC")
+if(isTrue){
+    console.log("Computer is blasting count till 3")
+}
+else{
+    console.log("Smart move dawg")
+}
+console.log("your number is" + a)
