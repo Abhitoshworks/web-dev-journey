668fb47ac244ed55039930bc5908ba48a7b59287
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-08-07 15:04:05 +0530

    filter

--- lesson-63p1/map.js
+++ lesson-63p1/map.js
@@ -6,4 +6,15 @@
 // }
 //   console.log(b); 
 //   ek tarika ye hai new array me element dalne ka
+ 
+let a = [1,2,3,4,5];
+let b = a.map((meow)=>{
+    return meow**2;
+   
+}
 
+);console.log(b);
+
+let d = [2,3,4,5,6];
+const isGreaterThenThree = (e)=> e>3;
+console.log(d.filter(isGreaterThenThree));
