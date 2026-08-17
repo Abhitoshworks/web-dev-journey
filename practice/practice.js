f358144de2cae1618d225bda2f89c344d71d5fd7
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-08-18 00:28:37 +0530

    factorial practice:

--- practice/practice.js
+++ practice/practice.js
@@ -1,4 +1,17 @@
+let a = 6;
+let factorial = 1;
+for(i=1; i<=a;i++){
+    factorial = factorial*i;
+}
+ console.log(factorial);
 
-let enemiesHP = [40, 150, 20, 200, 10];
-const greaterThen50 = (e) => e>50;
-console.log(enemiesHP.filter(greaterThen50));
+
+ // via reduced
+
+ let b = 6;
+ let c = [];
+ for(i=1; i<=b;i++){
+     c.push(i);
+ }
+let fact = (a,b) => a*b;
+ console.log(c.reduce(fact));
